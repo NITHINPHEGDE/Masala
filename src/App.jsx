77da1fc51756a270d1ae0070c59812926776c8c0
nithin p hegde <nithinphegde@gmail.com>
2025-07-29 12:01:35 +0530

    footer addedd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import Mainn from "./Mainn";
+import Footer from "./Footer";
 
 export default function App() {
   return (
     <>
       <Header />
       <Mainn></Mainn>
+      <Footer></Footer>
     </>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,5 @@
+export default function Footer(){
+  return(
+    <footer> <h1>Made with ❤️ by <strong>Nithin</strong></h1></footer>
+  )
+}
